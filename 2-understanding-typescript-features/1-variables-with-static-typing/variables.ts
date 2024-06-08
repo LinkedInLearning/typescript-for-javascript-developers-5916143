2a24dd988229d94d53bbd2145295ddab4a1b8b32
BrightBoost <info@brightboost.nl>
2024-06-08 16:51:19 +0200

    starter code

---
+++ 2-understanding-typescript-features/1-variables-with-static-typing/variables.ts
@@ -0,0 +1,13 @@
+let hello: string = "Hello, TypeScript!";
+let count: number = 42;
+let isLoggedIn: boolean = false; 
+
+let messageTs = "Hello, TypeScript!";  // Type inferred as string 
+
+let uncertain: any = 23;
+uncertain = "Suddenly a string";
+
+let numbers: number[] = [1, 2, 3, 4];  // Array of numbers
+
+enum Color { Red, Green, Blue };
+let c: Color = Color.Green;
