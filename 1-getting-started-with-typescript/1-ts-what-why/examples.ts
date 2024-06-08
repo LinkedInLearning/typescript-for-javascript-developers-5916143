29f1524f9bf90233cb4afb095ee4ce09546ac35d
BrightBoost <info@brightboost.nl>
2024-06-08 15:29:49 +0200

    examples from video

---
+++ 1-getting-started-with-typescript/1-ts-what-why/examples.ts
@@ -0,0 +1,16 @@
+// This is how to write it in JavaScript
+
+let name1 = "Zia";
+
+function add1(x, y) {
+  return x + y;
+}
+
+ 
+// This is how to write it in TypeScript
+
+let name2: string = "Zia";
+
+function add2(x: number, y: number): number {
+  return x + y;
+}
