25d486f5c1d8a9f55376b50c2c337f3d2d598dad
BrightBoost <info@brightboost.nl>
2024-06-11 12:48:58 +0200

    js app

---
+++ 3-practical-applications-of-typescript/1-migration-js-to-ts/app.js
@@ -0,0 +1,7 @@
+const { add, subtract } = require('./mathUtils');
+
+const result1 = add(10, 5);
+const result2 = subtract(10, 5);
+
+console.log('Addition:', result1);
+console.log('Subtraction:', result2);
